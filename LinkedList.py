1b608bc1dffcf39fd08481d84ee4b7f42f3400bb
ntIgnant <ignanttt@gmail.com>
2025-04-03 17:50:29 +0200

    UPDATED | linkedlist task started (didin't do shit ngl)

---
+++ LinkedList.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, data):
+        self.data = data # place to store the actual data (content)
+        self.next = None # place to store the 'pointer' to the next node
+
+
