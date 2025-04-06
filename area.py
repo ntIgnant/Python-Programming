b21e2bc4e43bbddbbf8b7aca351244f1bc23fda1
ntIgnant <ignanttt@gmail.com>
2025-04-06 21:59:45 +0200

    UPDATED | area.py and week16 folder modified

---
+++ area.py
@@ -0,0 +1,8 @@
+import math
+
+circle_diameter = 3.5
+circle_radius = circle_diameter / 2
+
+area = math.pi * pow(circle_radius, 2) # pow = (base^expo) so r^2 in this case
+
+print(area)
--- week16_material/word_counter.py
+++ week16_material/word_counter.py
@@ -1,3 +1,4 @@
+import collections
 import pathlib
 
 # Change this to "text" for the full book
@@ -7,4 +8,10 @@ file_name = "text_excerpt.txt"
 with (pathlib.Path(__file__).parent / file_name).open("rt", encoding="utf-8") as f:
     text = f.read()
 
-print(text)
+# print(text)
+'''
+Count which word appears how often and print the 20 most frequent words, and count
+which character appears how often and print their relative frequencies (see also Zipf’s law)
+'''
+
+collections.Counter()
