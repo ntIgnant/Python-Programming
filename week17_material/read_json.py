bbd435cd6178949c88bb0a354908e0b241a314bc
ntIgnant <ignanttt@gmail.com>
2025-04-14 11:09:06 +0200

    UPDATED | week17 content modified | workshops done related to csv and json

---
+++ week17_material/read_json.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+import json
+
+json_file_path = Path("users.json")
+
+with json_file_path.open("rt") as file:
+    content = json.load(file) # creates a json readable object
+
+    # print(json.dumps(content, indent=4)) # print the json file with nice looking (with nice indentation shit)
+
+    print(f"Users Online: ")
+    for user in content:
+        if user["online"] is True or user["online"] == "true":
+            print(user["username"]) # Not working, WTF? 
