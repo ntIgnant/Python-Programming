c213831d348a606fae31bf5b274dcbc03c61d177
ntIgnant <ignanttt@gmail.com>
2025-05-17 22:39:35 +0200

    UPDATED | things organized | python files moved to folders

--- GymData/main.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-import csv
-
-with Path("DataBase.csv").open("rt") as file:
-    content = csv.reader(file) # Open up the csv file in read (text mode) as list (Normal) Reader
-
-    for row in content:
-        print(row)
--- hack/tools/get_robots.py
+++ hack/tools/get_robots.py
@@ -16,17 +16,22 @@ user_url_robots = ''.join(split_user_url) + '/robots.txt'
 try:
     response = requests.get(user_url_robots, timeout=5)
     if response.status_code == 200:
-        print("Directory exists! | Data Fetched")
+        print("Directory exists! | Data Fetched\n")
         robots_data = response.text # Robots.txt content
 
     elif response.status_code == 403:
         print("Directory exists but access is forbidden.")
+        exit(0)
+
     elif response.status_code == 404:
         print("Directory does not exist.")
+        exit(0)
+
     else:
         print(f"Received unexpected status code: {response.status_code}")
+        exit(1)
 except requests.RequestException as e:
     print(f"Request failed: {e}")
 
-
+print("---- ROBOTS.TXT DATA ----")
 print(robots_data)
