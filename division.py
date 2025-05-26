4bb2d6e0f45be6803cb2667541fcc64885dd9848
ntIgnant <ignanttt@gmail.com>
2025-05-26 13:57:58 +0200

    UPDATED | stuff changed

---
+++ division.py
@@ -0,0 +1,11 @@
+# Difference between integer and float division
+
+num1 = 11
+num2 = 33
+
+result_float = num1 / num2
+
+result_int = num1 // num2
+
+print(f"Float result: {result_float} | Type: {type(result_float)}")
+print(f"Integer result: {result_int} | Type: {type(result_int)}")
--- lists.py
+++
@@ -1,7 +0,0 @@
-my_list = [x for x in range(1, 11)]
-
-number_str = "Number: "
-
-number_list = [number_str + str(num) for num in my_list if num % 2 == 0] # list containing 'Number: x' for x if is even num
-
-print(number_list)
