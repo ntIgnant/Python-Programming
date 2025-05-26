5a51f9d6ce44efc8a43d62496ecd162e9bfa8e78
ntIgnant <ignanttt@gmail.com>
2025-05-26 12:53:04 +0200

    UPDATED | equality task added | structureal vs referential equality

---
+++ equality.py
@@ -0,0 +1,8 @@
+numList1 = [1, 2, 3, 4]
+numList2 = [1, 2, 3, 4]
+
+print(
+    numList1 == numList2
+)  # structureal equality | when the actual content is the same
+
+print(numList1 is numList2)  # referential equality
