8c4725e84fd3a210a4560ff1e876ceadb5ba74a8
ntIgnant <ignanttt@gmail.com>
2025-06-01 22:20:13 +0200

    UPDATED | Exam prep tryouts

---
+++ Exam/exam_tryout.py
@@ -0,0 +1,20 @@
+#print(len(0))
+#print(len(""))
+
+#a = 5
+#b = 5
+
+#c = [1, 2, 3]
+#d = [1, 2, 3]
+
+#print(a == b)
+#print(a is b)
+#print("")
+#print(c == d)
+#print(c is d)
+
+#l = [i % 4 for i in range(10) if i % 3]
+#print(l)
+
+p = [i % 2 for i in range(10) if i % 3]
+print(p)
