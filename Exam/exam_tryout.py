5e6aab06de4666e88990026b34b2abcdb2657dd8
ntIgnant <ignanttt@gmail.com>
2025-06-04 16:54:48 +0200

    UPDATED | exam median exercise done | exam tryout

--- Exam/exam_tryout.py
+++ Exam/exam_tryout.py
@@ -19,8 +19,23 @@
 #p = [i % 2 for i in range(10) if i % 3]
 #print(p)
 
-j = [i for i in "abac"]
-print(j)
+#j = [i for i in "abac"]
+#print(j)
 
-set_j = set(j)
-print(set_j)
+#set_j = set(j)
+#print(set_j)
+
+# Get the median of an odd progresion?
+mi_lista = [1, 5, 2, 4, 1, 6, 3]
+sorted_lista = sorted(mi_lista) # sort the list
+
+print(sorted_lista) # DEBUG
+
+# Now, I should get the term in the middle
+len_lista = len(sorted_lista)
+
+mid_index = (len_lista // 2) + 1 # perform integer divison and then add one to get the follow-up value
+
+median = sorted_lista[mid_index - 1]
+
+print(median)
